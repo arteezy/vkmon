3c51fd55011971c7637c682635721dddcbffc99f
Arthur L <mardybm@gmail.com>
2011-08-28 00:26:40 +0700

    using haml and css

--- vk.rb
+++ vk.rb
@@ -1,17 +1,27 @@
+require 'sinatra'
+require 'haml'
 require 'net/https'
 require 'json'
 require 'uri'
 
-id = 3727331
-#id = 2917062
+helpers do
+  def mon
+    id = 3727331
 
-myreq = 'https://api.vkontakte.ru/method/getProfiles?uid=' + id.to_s + '&fields=online,domain,rate,bdate'
+    myreq = 'https://api.vkontakte.ru/method/getProfiles?uid=' + id.to_s + '&fields=online,domain,rate,bdate'
 
-uri = URI.parse(myreq)
-http = Net::HTTP.new(uri.host, uri.port)
-http.use_ssl = true
+    uri = URI.parse(myreq)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
 
-request = Net::HTTP::Get.new(uri.request_uri)
-response = http.request(request)
-ans = JSON.parse(response.body.slice(13..-3))
-ans.each{|key,value| puts "#{key} = #{value}"}
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    ans = JSON.parse(response.body.slice(13..-3)) 	 
+    return ans
+  end
+end
+
+get '/' do
+  @lol = mon
+  haml :index
+end
