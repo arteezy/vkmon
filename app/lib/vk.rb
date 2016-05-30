e67711677a874c3e55f17a033dd2f94acd91a84f
Arthur L <mardybm@gmail.com>
2016-05-30 20:15:56 +0600

    add VK api endpoint for requesting friendlist

---
+++ app/lib/vk.rb
@@ -0,0 +1,16 @@
+class VK
+  include HTTParty
+  base_uri 'https://api.vk.com/method'
+  default_params v: '5.58'
+
+  def initialize(user_id)
+    @options = { user_id: user_id }
+  end
+
+  def friends(method = :friends, order = :name)
+    fields = %w(nickname domain sex bdate city country photo_200_orig has_mobile
+                contacts education online relation last_seen status universities)
+    query = @options.merge(order: order, fields: fields.join(','))
+    self.class.get("/#{method}.get", query: query)
+  end
+end
