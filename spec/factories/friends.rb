a52b0ba0c5521d092130eeac5171892de5d0e9bb
Arthur L <mardybm@gmail.com>
2016-06-06 00:06:57 +0600

    add proper factory for Friend model

--- spec/factories/friends.rb
+++ spec/factories/friends.rb
@@ -1,21 +1,25 @@
 FactoryGirl.define do
   factory :friend do
-    first_name "MyString"
-    last_name "MyString"
-    sex false
-    nickname "MyString"
-    domain "MyString"
-    bdate "2016-11-01"
-    city "MyString"
-    country "MyString"
-    photo "MyString"
-    has_mobile false
-    online false
-    deactivated "MyString"
-    status "MyString"
-    last_seen "2016-11-01 04:40:44"
-    platform 1
-    university "MyString"
-    graduation "MyString"
+    id { Faker::Number.number(7) }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    sex { Faker::Boolean.boolean }
+    nickname { Faker::Internet.user_name }
+    domain { Faker::Internet.domain_word }
+    bdate { Faker::Date.between(40.years.ago, 16.years.ago) }
+    city { Faker::Address.city }
+    country { Faker::Address.country }
+    photo { Faker::Avatar.image }
+    has_mobile { Faker::Boolean.boolean }
+    online { Faker::Boolean.boolean }
+    status { Faker::Lorem.sentence }
+    last_seen { Faker::Time.backward(3, :morning) }
+    platform { Faker::Number.digit }
+    university { Faker::University.name }
+    graduation { Faker::Number.between(1970, 2038) }
+
+    factory :banned_friend do
+      deactivated 'banned'
+    end
   end
 end
--- spec/models/friend_spec.rb
+++ spec/models/friend_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Friend, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(build(:friend)).to be_valid
+  end
 end
