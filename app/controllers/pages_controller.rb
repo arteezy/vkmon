d0541890c658906d27811d8b7f0a36b65a386ab0
Arthur L <mardybm@gmail.com>
2016-07-17 01:26:08 +0600

    add status page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def status
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   resources :watchers
+  get 'status', to: 'pages#status'
 end
