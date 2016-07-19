11d0637124a0b10cc518d0c844cb9e14643dded0
Arthur L <mardybm@gmail.com>
2016-07-20 01:05:30 +0600

    move status page logic out of view layer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,19 @@
 class PagesController < ApplicationController
+  before_action :require_login, only: :status
+
   def home
   end
 
   def status
+    @status = {
+      ruby:          RUBY_VERSION,
+      rails:         Rails.version,
+      time:          Time.now.to_s(:long),
+      puma:          Gem.loaded_specs['puma'].version,
+      postgresql:    User.connection.select_value('SHOW server_version'),
+      user_count:    User.count,
+      watcher_count: Watcher.count,
+      friend_count:  Friend.count
+    }
   end
 end
