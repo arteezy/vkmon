a5ae22a43673795f9690c25740c89bc034a9e015
Arthur L <mardybm@gmail.com>
2016-07-20 02:02:42 +0600

    add temporary HTTP Basic Auth to protect app before it's finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  http_basic_authenticate_with name: ENV.fetch('ADMIN_EMAIL'), password: ENV.fetch('ADMIN_PASSWORD'), except: :about if Rails.env.production?
   include Clearance::Controller
 end
