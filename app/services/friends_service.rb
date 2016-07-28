c2d5cdd4a127b8bf9bd7ef395c2b0e6b2490c6e9
Arthur L <mardybm@gmail.com>
2016-07-28 17:26:43 +0600

    move fetch logic from job to service

---
+++ app/services/friends_service.rb
@@ -0,0 +1,32 @@
+class FriendsService
+  def initialize(watcher)
+    @watcher = watcher
+  end
+
+  def fetch
+    vk = VK.new(@watcher.vk_id)
+    friends = vk.friends['response']['items']
+    old_friends = @watcher.friends.pluck(:id)
+    new_friends = friends.map { |i| i['id'] }
+
+    save_friends(friends)
+
+    return if old_friends.empty?
+    @watcher.added_friends   |= new_friends - old_friends
+    @watcher.deleted_friends |= old_friends - new_friends
+    @watcher.save!
+  end
+
+  private
+
+  def save_friends(friends)
+    Friend.transaction do
+      friends.each do |friend|
+        new_friend = Friend.find_or_initialize_by(id: friend['id'])
+        new_friend.map_attributes_from_api(friend)
+        new_friend.watchers |= [@watcher]
+        new_friend.save!
+      end
+    end
+  end
+end
--- spec/jobs/fetch_friends_job_spec.rb
+++ spec/jobs/fetch_friends_job_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe FetchFriendsJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'matches with enqueued job' do
+    ActiveJob::Base.queue_adapter = :test
+    watcher = FactoryGirl.create(:watcher)
+    expect {
+      FetchFriendsJob.perform_later(watcher)
+    }.to have_enqueued_job.with(watcher)
+  end
 end
