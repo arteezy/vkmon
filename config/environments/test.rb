5651c150f7a594a8b13028c8578ac82b0e4a8ccc
Arthur L <mardybm@gmail.com>
2016-08-05 14:50:51 +0600

    set proper environment for testing ActiveJob related code

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -35,6 +35,9 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  # Enqueue jobs, but don't actually perform them
+  config.active_job.queue_adapter = :test
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
--- spec/jobs/fetch_friends_job_spec.rb
+++ spec/jobs/fetch_friends_job_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe FetchFriendsJob, type: :job do
+  let(:watcher) { FactoryGirl.create(:watcher) }
+
+  it 'queues the job' do
+    expect {
+      FetchFriendsJob.perform_later(watcher)
+    }.to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
+  end
+
   it 'matches with enqueued job' do
-    ActiveJob::Base.queue_adapter = :test
-    watcher = FactoryGirl.create(:watcher)
     expect {
       FetchFriendsJob.perform_later(watcher)
     }.to have_enqueued_job.with(watcher)
