a13dbb41beb4f0e51528b75e25e0a7ffcbc811c1
Arthur L <mardybm@gmail.com>
2016-08-06 15:04:18 +0600

    add 100% covered feature tests for watchers

--- spec/features/watchers/user_interacts_with_watchers_spec.rb
+++ spec/features/watchers/user_interacts_with_watchers_spec.rb
@@ -4,9 +4,34 @@ require 'support/features/clearance_helpers'
 RSpec.feature 'User interacts with watchers' do
   given(:user) { FactoryGirl.create(:user) }
 
-  scenario 'opens his own watcher' do
+  before(:each) do
     sign_in_with(user.email, user.password)
+  end
 
+  scenario 'creates new watcher' do
+    visit watchers_path
+
+    fill_in 'VK ID', with: rand(1000)
+    fill_in 'Full Name', with: 'John Doe'
+    fill_in 'Photo', with: 'https://example.com/image.png'
+    click_button 'Create'
+
+    expect(page).to have_content 'Successfully created watcher'
+    expect(page).to have_content 'John Doe'
+  end
+
+  scenario 'fails to create new watcher' do
+    visit watchers_path
+
+    fill_in 'Full Name', with: 'John Doe'
+    fill_in 'Photo', with: 'https://example.com/image.png'
+    click_button 'Create'
+
+    expect(page).to have_content 'Unable to create watcher!'
+    expect(page).not_to have_content 'John Doe'
+  end
+
+  scenario 'opens his own watcher' do
     watcher = FactoryGirl.create(:watcher, user: user)
 
     visit watcher_path(watcher)
@@ -15,13 +40,22 @@ RSpec.feature 'User interacts with watchers' do
   end
 
   scenario 'tries to open someone\'s other watcher' do
-    sign_in_with(user.email, user.password)
-
     other_user = FactoryGirl.create(:user)
     other_watcher = FactoryGirl.create(:watcher, user: other_user)
 
     visit watcher_path(other_watcher)
 
     expect(page).to have_content 'You can access only your own watchers!'
+  end
+
+  scenario 'destroys watcher' do
+    watcher = FactoryGirl.create(:watcher, user: user)
+
+    visit watchers_path
+
+    click_link 'Destroy'
+
+    expect(page).to have_content 'Watcher was successfully destroyed'
+    expect(page).not_to have_content watcher.name
   end
 end
