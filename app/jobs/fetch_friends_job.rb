bcdbe1ba54d9924b73f9bd7e03dab870722c539d
Arthur L <mardybm@gmail.com>
2016-08-08 21:36:27 +0600

    move to inline adapter for jobs from rake tasks to run inside one-off dynos on Heroku

--- app/jobs/fetch_friends_job.rb
+++ app/jobs/fetch_friends_job.rb
@@ -2,6 +2,10 @@ class FetchFriendsJob < ApplicationJob
   queue_as :default
 
   def perform(watcher)
+    puts watcher.id
     FriendsService.new(watcher).fetch
+  rescue => e
+    ActiveJob::Base.logger.error e
+    raise e
   end
 end
